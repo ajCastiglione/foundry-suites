957c280366604dd6ea98b8d009e270fa418405fd
ajCastiglione <antoniocastiglione10@gmail.com>
2019-10-18 11:52:49 -0400

    started to tweak widget code, will use hidden fields to send to app

--- src/partials/Widget.js
+++ src/partials/Widget.js
@@ -2,38 +2,49 @@ import React, { Component } from "react";
 
 export default class Widget extends Component {
   state = {
-    CID: ""
+    "check-in": "",
+    "check-out": ""
   };
 
-  valueUpdate = e => {
+  handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  render() {
+  getToday = () => {
     let date = new Date();
     let month = date.getMonth() + 1;
     let formattedMonth = month.toString().length < 2 ? "0" + month : month;
-    let today =
-      date.getFullYear() + "-" + formattedMonth + "-" + date.getDate();
+    return date.getFullYear() + "-" + formattedMonth + "-" + date.getDate();
+  };
+
+  render() {
+    let today = this.getToday();
+
     return (
       <form
         action="https://us01.iqwebbook.com/FSBBUF735/Integration/Search"
         method="POST"
-        name="SearchForm">
+        name="SearchForm"
+      >
         <div className="form-group">
           <label htmlFor="AD">Adults</label>
           <input type="number" max="4" min="1" name="AD" />
           <label htmlFor="CH">Children</label>
           <input type="number" max="3" min="1" name="CH" />
-          <label htmlFor="CID">Check-in Day</label>
+          <label htmlFor="check-in">Check-in Day</label>
           <input
             type="date"
             min={today}
-            name="CID"
-            onChange={this.valueUpdate}
+            name="check-in"
+            onChange={this.handleChange}
           />
-          <label htmlFor="COD">Check-out Day</label>
-          <input type="date" min={this.state.CID} name="COD" />
+          <label htmlFor="check-out">Check-out Day</label>
+          <input
+            type="date"
+            min={this.state.CID}
+            name="check-out"
+            onChange={this.handleChange}
+          />
         </div>
         <input type="hidden" value={this.state.CID} />
         <input type="hidden" name="UF1" />
